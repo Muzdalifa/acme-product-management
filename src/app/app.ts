76203ddf31ca3db1f21a933986e82158ba452cbd
Muzdalifa Ali <muzdalifa.ali@addgile.com>
2025-07-19 16:51:52 +0200

    add product component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductSelection } from "./products/product-selection/product-selection";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductSelection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
